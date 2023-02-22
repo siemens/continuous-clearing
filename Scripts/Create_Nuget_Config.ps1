816ce4920c3acc847a93e47c3591a02bc5483798
MadanReddyK <125262006+MadanReddyK@users.noreply.github.com>
2023-02-22 20:49:24 +0530

    Add files via upload

---
+++ Scripts/Create_Nuget_Config.ps1
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2023 Siemens AG
+# SPDX-License-Identifier: MIT
+
+param(
+	$energydevnuget="xyz",
+	$energydevnugetegll="xyz")
+
+$nugetconfig = "$PSScriptRoot\..\nuget.config"
+
+Write-Host "Remove existing Nuget Config if any"
+if (Test-Path "$nugetconfig") 
+{
+	Remove-Item "$nugetconfig" -Force
+}
+
+New-Item -ItemType File -Force -Path "$nugetconfig"
+
+Write-Host "The energy dev nuget URL is      : $energydevnuget"
+Write-Host "The energy dev nuget egll  URL is: $energydevnugetegll"
+
+Write-Host "Write base config"
+
+$xmlBase = @'
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+ <packageSources>
+    <add key="energy-dev-nuget" value="$energydevnuget" />
+    <add key="energy-dev-nuget-egll" value="$egllenergydevnuget" />
+ </packageSources>
+</configuration>
+'@
+
+$xmltemp = ($xmlBase.Replace('$energydevnuget', "$energydevnuget")).Replace('$egllenergydevnuget', "$energydevnugetegll")
+#$xmlBase.Replace('$egllenergydevnuget', "$egllenergydevnuget")
+Set-Content -Path "$nugetconfig" -Value $xmltemp
+
+<# 
+$tempXMLcont = Get-Content "$nugetconfig"
+$tempXMLcont.replace('$energydevnuget',"$energydevnuget") | set-content "$nugetconfig" -force
+
+$tempXMLcont = Get-Content "$nugetconfig"
+$tempXMLcont.replace('$egllenergydevnuget',"$energydevnugetegll") | set-content "$nugetconfig" -force #>
---
+++ Scripts/docker_scripts/sw360_instance_referesh.ps1
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 Siemens AG
+# SPDX-License-Identifier: MIT
+
+docker exec -dt sw360confctnr1 service postgresql stop
+docker stop sw360confctnr1
+docker rm sw360confctnr1
+Write-Host "List of containers BEFORE sw360 start"
+docker ps -a
+docker run --name sw360confctnr1 --memory=5g -dt -p ${env:SW360APPPORT}:8080 -p 5985:5984 -p 5435:5432 ${env:DOCKERDEVARTIFACTORY}/energy-dev/software-clearing/sw360/sw360conf:latest
+Start-Sleep -s 540
+Write-Host "List of containers AFTER sw360 start"
+docker ps -a
