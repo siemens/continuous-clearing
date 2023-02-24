2990be4320ef7dc9c7f7ffdc77b356665867ec9c
MadanReddyK <125262006+MadanReddyK@users.noreply.github.com>
2023-02-24 16:35:14 +0530

    Update sw360_instance_referesh.ps1

--- src/Scripts/docker_scripts/sw360_instance_referesh.ps1
+++ src/Scripts/docker_scripts/sw360_instance_referesh.ps1
@@ -6,7 +6,7 @@ docker stop sw360confctnr1
 docker rm sw360confctnr1
 Write-Host "List of containers BEFORE sw360 start"
 docker ps -a
-docker run --name sw360confctnr1 --memory=5g -dt -p ${env:SW360APPPORT}:8080 -p 5985:5984 -p 5435:5432 ${env:DOCKERDEVARTIFACTORY}/energy-dev/software-clearing/sw360/sw360conf:latest
+docker run --name sw360confctnr1 --memory=5g -t -d -p ${env:SW360APPPORT}:8080 -p 5985:5984 -p 5435:5432 ${env:DOCKERDEVARTIFACTORY}/energy-dev/software-clearing/sw360/sw360conf:latest
 Start-Sleep -s 540
 Write-Host "List of containers AFTER sw360 start"
 docker ps -a
