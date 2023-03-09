05fc3d27e2fc0c5a5250e83d0fd21401212ed3c4
Sumanth K B <sumanth.k-b@siemens.com>
2023-03-09 15:15:01 +0530

    maven Dev dependency changes

--- src/LCT.PackageIdentifier.UTest/MavenParserTests.cs
+++ src/LCT.PackageIdentifier.UTest/MavenParserTests.cs
@@ -8,7 +8,9 @@
 using CycloneDX.Models;
 using LCT.Common;
 using LCT.Common.Model;
+using LCT.PackageIdentifier.Model;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.IO;
 
 namespace LCT.PackageIdentifier.UTest
@@ -31,9 +33,6 @@ namespace LCT.PackageIdentifier.UTest
                 ProjectType = "MAVEN",
                 RemoveDevDependency = true,
                 Maven = new Config() { Include = Includes, Exclude = Excludes }
-
-
-
             };
 
             MavenProcessor MavenProcessor = new MavenProcessor();
@@ -44,6 +43,33 @@ namespace LCT.PackageIdentifier.UTest
             //Assert
             Assert.That(3, Is.EqualTo(bom.Components.Count), "Returns the count of components");
 
+        }
+
+        [TestCase]
+        public void IsDevDependent_GivenListOfMavenDevComponents_ReturnsNonDevComponents()
+        {
+            //Arrange
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string outFolder = Path.GetDirectoryName(exePath);
+            string filepath = outFolder + @"\PackageIdentifierUTTestFiles";
+            string[] Includes = { "POM.xml" };
+            string[] Excludes = { "lol" };
+
+            CommonAppSettings appSettings = new CommonAppSettings()
+            {
+                PackageFilePath = filepath,
+                ProjectType = "MAVEN",
+                RemoveDevDependency = true,
+                Maven = new Config() { Include = Includes, Exclude = Excludes, DevDependentScopeList = new string[] { "test" } }
+            };
+
+            MavenProcessor MavenProcessor = new MavenProcessor();
+
+            //Act
+            Bom bom = MavenProcessor.ParsePackageFile(appSettings);
+
+            //Assert
+            Assert.That(1, Is.EqualTo(bom.Components.Count), "Returns the count of NON Dev Dependency components");
         }
     }
 }
