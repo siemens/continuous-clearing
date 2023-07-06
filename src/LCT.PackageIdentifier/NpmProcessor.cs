7c2e0f46b5eb532ecde76a2785f479304d044295
Sumanth K B <sumanth.k-b@siemens.com>
2023-07-06 12:35:33 +0530

    Npm Change

--- src/LCT.PackageIdentifier/NpmProcessor.cs
+++ src/LCT.PackageIdentifier/NpmProcessor.cs
@@ -353,20 +353,7 @@ namespace LCT.PackageIdentifier
                     componentsForBOM.AddRange(lst);
                 }
             }
-            else
-            {
-                configFiles = FolderScanner.FileScanner(appSettings.CycloneDxBomFilePath, appSettings.Npm);
-                foreach (string filepath in configFiles)
-                {
-                    Bom bomList = ParseCycloneDXBom(filepath);
-                    componentsForBOM.AddRange(bomList.Components);
-                }
-                bom.Components = componentsForBOM;
-                BomCreator.bomKpiData.ComponentsinPackageLockJsonFile = bom.Components.Count;
-                bom = RemoveExcludedComponents(appSettings, bom);
-
-                componentsForBOM = bom.Components;
-            }
+            BomCreator.bomKpiData.ComponentsinPackageLockJsonFile = count;
         }
 
         private static bool IsDevDependency(JToken devValue, ref int noOfDevDependent)
