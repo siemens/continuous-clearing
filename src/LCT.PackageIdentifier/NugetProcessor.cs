773f82195e6b9be22367e2ffb0dac839d1e5a8ba
Chalapala RaghavendraReddy <raghavendrareddy.chalapala@siemens.com>
2023-08-25 19:21:10 +0530

    sonarissuefixed

--- src/LCT.PackageIdentifier/NugetProcessor.cs
+++ src/LCT.PackageIdentifier/NugetProcessor.cs
@@ -654,9 +654,9 @@ namespace LCT.PackageIdentifier
         }
         public static void GetDependencyList(KeyValuePair<string,BuildInfoComponent> lst,ref List<string> depvalue)
         {
-            if (lst.Value!=null)
+            if (lst.Value.Dependencies !=null)
             {
-                foreach (var item in lst.Value.Dependencies)
+            foreach (var item in lst.Value.Dependencies)
                 {
                     var depvaltestue = item.PackageUrl;
                     depvalue.Add(depvaltestue);
