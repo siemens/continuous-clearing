958befcf71433da87deb8c0d511ec73c7e63f5ed
Aditya Narayan <narayanpaditya@gmail.com>
2023-12-07 12:47:31 +0530

    syntax error

--- src/LCT.SW360PackageCreator/CreatorHelper.cs
+++ src/LCT.SW360PackageCreator/CreatorHelper.cs
@@ -229,7 +229,7 @@ namespace LCT.SW360PackageCreator
                     mapper.DownloadUrl = GetMavenDownloadUrl(mapper, item, releasesInfo);
                 }
                 else if (!string.IsNullOrEmpty(item.ReleaseExternalId) && 
-                            (item.ReleaseExternalId.Contains(Dataconstant.PurlCheck()["PYTHON"]) || item.ReleaseExternalId.Contains(Dataconstant.PurlCheck()["CONAN"]))
+                            (item.ReleaseExternalId.Contains(Dataconstant.PurlCheck()["PYTHON"]) || item.ReleaseExternalId.Contains(Dataconstant.PurlCheck()["CONAN"])))
                 {
                     mapper.DownloadUrl = mapper.SourceUrl;
                 }
