6f93dfc2567f8883ae9157e8b65cf61b1d00451e
Chalapala RaghavendraReddy <raghavendrareddy.chalapala@siemens.com>
2024-02-27 10:19:07 +0530

    Code changes for classification of packages

--- src/ArtifactoryUploader/Model/UnknownPackagesAll.cs
+++ src/ArtifactoryUploader/Model/UnknownPackagesAll.cs
@@ -23,6 +23,24 @@ namespace LCT.ArtifactoryUploader.Model
         public List<ComponentsToArtifactory> UnknownPackagesPython { get; set; }
         public List<ComponentsToArtifactory> UnknownPackagesDebian { get; set; }
         public List<ComponentsToArtifactory> UnknownPackagesMaven { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesNpm { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesNuget { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesConan { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesPython { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesDebian { get; set; }
+        public List<ComponentsToArtifactory> JfrogNotFoundPackagesMaven { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesNpm { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesNuget { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesConan { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesPython { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesDebian { get; set; }
+        public List<ComponentsToArtifactory> JfrogFoundPackagesMaven { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesNpm { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesNuget { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesConan { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesPython { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesDebian { get; set; }
+        public List<ComponentsToArtifactory> SuccessfullPackagesMaven { get; set; }
 
     }
 }
