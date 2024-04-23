c51028e3e1410df102afacf4c312001cd2dc92e5
Sumanth Kb <sumanth.k-b@siemens.com>
2024-04-23 15:36:49 +0530

    Dependency of ProjectType in Creator

--- src/LCT.SW360PackageCreator/Program.cs
+++ src/LCT.SW360PackageCreator/Program.cs
@@ -55,10 +55,6 @@ namespace LCT.SW360PackageCreator
 
             Logger.Logger.Log(null, Level.Notice, $"\n====================<<<<< Package creator >>>>>====================", null);
             Logger.Logger.Log(null, Level.Notice, $"\nStart of Package creator execution : {DateTime.Now}", null);
-            if (appSettings.ProjectType?.ToUpperInvariant() == "ALPINE")
-            { 
-                Logger.Error($"\nPlease note that the Alpine feature is currently in preview state. This means it's available for testing and evaluation purposes. While functional, it may not yet include all planned features and could encounter occasional issues. Your feedback during this preview phase is appreciated as we work towards its official release. Thank you for exploring Alpine with us.");
-            }
 
             if (appSettings.IsTestMode)
                 Logger.Logger.Log(null, Level.Alert, $"Package creator is running in TEST mode \n", null);
