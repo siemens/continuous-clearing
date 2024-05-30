08ad099f52b68c64f18b9e70168fc96a0a81e244
karthika <karthikag1810@gmail.com>
2024-05-30 18:00:47 +0530

    bug fix

--- src/LCT.APICommunications/JfrogAqlApiCommunication.cs
+++ src/LCT.APICommunications/JfrogAqlApiCommunication.cs
@@ -58,7 +58,7 @@ namespace LCT.APICommunications
             StringBuilder query = new();
             query.Append("items.find({\"repo\":\"");
             query.Append($"{repoName}");
-            query.Append("\"}).include(\"repo\", \"path\", \"name\")");
+            query.Append("\"}).include(\"repo\", \"path\", \"name\", \"actual_sha1\",\"actual_md5\",\"sha256\")");
 
             string aqlQueryToBody = query.ToString();
             string uri = $"{DomainName}{ApiConstant.JfrogArtifactoryApiSearchAql}";
@@ -119,7 +119,7 @@ namespace LCT.APICommunications
             }
 
             StringBuilder query = new();
-            query.Append($"items.find({{{string.Join(", ", queryList)}}}).include(\"repo\", \"path\", \"name\", \"actual_sha1\",\"actual_md5\",\"sha256\").limit(1)");
+            query.Append($"items.find({{{string.Join(", ", queryList)}}}).include(\"repo\", \"path\", \"name\").limit(1)");
 
             return query.ToString();
         }
