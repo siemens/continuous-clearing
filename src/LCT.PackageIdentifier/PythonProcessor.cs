1cbc34f69d7fa5378802af17de351b65c375fb02
MalavikaKrishnan400 <malavikakrishnan400@gmail.com>
2024-11-27 18:21:20 +0530

    bug fixes (#209)
    * bug fixes
    
    * Resolve review comments
    
    * Changed Poetry lock Dev Dependency identification logic.
    
    * Updated to 7.0.1 in all the project files

--- src/LCT.PackageIdentifier/PythonProcessor.cs
+++ src/LCT.PackageIdentifier/PythonProcessor.cs
@@ -130,7 +130,8 @@ namespace LCT.PackageIdentifier
                     Name = node["name"].ToString(),
                     Version = node["version"].ToString(),
                     PurlID = Dataconstant.PurlCheck()["PYTHON"] + "/" + node["name"].ToString() + "@" + node["version"].ToString(),
-                    Isdevdependent = node["category"].ToString() != "main",
+                    // By Default Tommy.TomlLazy is coming instead of Null or empty
+                    Isdevdependent = (node["category"].ToString() != "main" && node["category"].ToString() != "Tommy.TomlLazy"),
                     FoundType = Dataconstant.Discovered
                 };
 
