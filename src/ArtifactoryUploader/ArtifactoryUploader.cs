31d43b655c27dbc43ce0c10ab3e346d70618651a
Aditya Narayan <narayan@siemens.com>
2025-01-18 11:51:08 +0530

    clean-up

--- src/ArtifactoryUploader/ArtifactoryUploader.cs
+++ src/ArtifactoryUploader/ArtifactoryUploader.cs
@@ -12,7 +12,6 @@ using LCT.ArtifactoryUploader.Model;
 using LCT.Services.Interface;
 using log4net;
 using System;
-using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
