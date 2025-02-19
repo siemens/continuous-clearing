27276d1fd41eae0f1959227e70c3443b8a44c1f3
Chalapala RaghavendraReddy <raghavendrareddy.chalapala@siemens.com>
2025-02-19 13:02:56 +0530

    Updated code changes

--- src/LCT.APICommunications/RetryHttpClientHandler.cs
+++ src/LCT.APICommunications/RetryHttpClientHandler.cs
@@ -16,21 +16,15 @@ namespace LCT.APICommunications
         {
             // Define the retry policy (retry on 5xx, 408, and transient errors)
             _retryPolicy = Policy
-                .Handle<HttpRequestException>()   // Handle HttpRequestException
-                .Or<TaskCanceledException>()     // Handle TaskCanceledException (timeouts)
+                .Handle<HttpRequestException>() 
+                .Or<TaskCanceledException>()    
                 .OrResult<HttpResponseMessage>(r =>
-                    (r.StatusCode == HttpStatusCode.RequestTimeout // 408 Request Timeout
-                    || (int)r.StatusCode >= 500) // 5xx server errors
-                    && r.StatusCode != HttpStatusCode.Unauthorized // 401 Unauthorized
-                    && r.StatusCode != HttpStatusCode.Forbidden) // 403 Forbidden
-                .WaitAndRetryAsync(ApiConstant.APIRetryCount, // Retry up to 3 times
-                    attempt => attempt switch
-                    {
-                        1 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalFirst),  // 1st retry after 5 seconds
-                        2 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalSecond), // 2nd retry after 10 seconds
-                        3 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalThird), // 3rd retry after 30 seconds
-                        _ => TimeSpan.Zero // Default (not used)
-                    },
+                    (r.StatusCode == HttpStatusCode.RequestTimeout
+                    || (int)r.StatusCode >= 500) 
+                    && r.StatusCode != HttpStatusCode.Unauthorized
+                    && r.StatusCode != HttpStatusCode.Forbidden)
+                .WaitAndRetryAsync(ApiConstant.APIRetryCount,
+                     GetRetryInterval,
                     onRetry: (outcome, timespan, attempt, context) =>
                     {
                         Logger.Debug($"Retry attempt {attempt} due to: {(outcome.Exception != null ? outcome.Exception.Message : $"{outcome.Result.StatusCode}")}");
@@ -47,20 +41,24 @@ namespace LCT.APICommunications
         {
             var retryPolicy = Policy
                 .Handle<WebException>()
-                .WaitAndRetryAsync(ApiConstant.APIRetryCount, // Retry up to 3 times
-                    attempt => attempt switch
-                    {
-                        1 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalFirst),  // 1st retry after 5 seconds
-                        2 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalSecond), // 2nd retry after 10 seconds
-                        3 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalThird), // 3rd retry after 30 seconds
-                        _ => TimeSpan.Zero // Default (not used)
-                    },
+                .WaitAndRetryAsync(ApiConstant.APIRetryCount,
+                    GetRetryInterval,
                     onRetry: (exception, timespan, attempt, context) =>
                     {
                         Logger.Debug($"Retry attempt {attempt} due to: {exception?.Message ?? "No exception"}");
                     });
 
             await retryPolicy.ExecuteAsync(action);
+        }
+        private static TimeSpan GetRetryInterval(int attempt)
+        {
+            return attempt switch
+            {
+                1 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalFirst),  // 1st retry after 5 seconds
+                2 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalSecond), // 2nd retry after 10 seconds
+                3 => TimeSpan.FromSeconds(ApiConstant.APIRetryIntervalThird), // 3rd retry after 30 seconds
+                _ => TimeSpan.Zero // Default (not used)
+            };
         }
     }
 }
