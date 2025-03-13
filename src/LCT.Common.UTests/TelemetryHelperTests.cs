07b8bbbced307acc7a81e7a94773ccace8b9a13a
Chalapala RaghavendraReddy <raghavendrareddy.chalapala@siemens.com>
2025-03-13 14:23:43 +0530

    missed file

--- src/LCT.Common.UTests/TelemetryHelperTests.cs
+++ src/LCT.Common.UTests/TelemetryHelperTests.cs
@@ -40,21 +40,21 @@ namespace LCT.Common.UTest
             Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
 
         }
-        //[Test]
-        //public void StartTelemetry_ShouldInitializeAndTrackEvent_WhenTelemetryIsEnabled()
-        //{
-        //    // Arrange
-        //    string catoolVersion = "1.0.0";
-        //    var kpiData = new { Metric1 = 100, Metric2 = 200 }; // Example KPI data
-        //    string telemetryFor = "TestEvent";
-        //    var consoleOutput = new System.IO.StringWriter();
-        //    Console.SetOut(consoleOutput);
-        //    // Act
-        //    // This would normally start telemetry tracking in your system
-        //    telemetryHelper.StartTelemetry(catoolVersion, kpiData, telemetryFor);
+        [Test]
+        public void StartTelemetry_ShouldInitializeAndTrackEvent_WhenTelemetryIsEnabled()
+        {
+            // Arrange
+            string catoolVersion = "1.0.0";
+            var kpiData = new { Metric1 = 100, Metric2 = 200 }; // Example KPI data
+            string telemetryFor = "TestEvent";
+            var consoleOutput = new System.IO.StringWriter();
+            Console.SetOut(consoleOutput);
+            // Act
+            // This would normally start telemetry tracking in your system
+            telemetryHelper.StartTelemetry(catoolVersion, kpiData, telemetryFor);
 
-        //    string output = consoleOutput.ToString();
-        //    Assert.AreEqual(output,"");
-        //}
+            string output = consoleOutput.ToString();
+            Assert.AreEqual(output, "");
+        }
     }
 }
