a2edb15657ac3f453160d3a941c69c00836a105d
Chalapala RaghavendraReddy <raghavendrareddy.chalapala@siemens.com>
2025-07-01 16:09:23 +0530

    today work

---
+++ src/LCT.Common/Interface/ISpdxBomParser.cs
@@ -0,0 +1,14 @@
+﻿using CycloneDX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCT.Common.Interface
+{
+    public interface ISpdxBomParser
+    {
+        public Bom ParseSPDXBom(string filePath);
+    }
+}
---
+++ src/LCT.Common/SpdxBomParser.cs
@@ -0,0 +1,21 @@
+﻿// Ignore Spelling: LCT Spdx
+
+using CycloneDX.Models;
+using LCT.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCT.Common
+{
+    public class SpdxBomParser : ISpdxBomParser
+    {
+        public Bom ParseSPDXBom(string filePath)
+        {
+            Bom bom = new Bom();
+            return bom;
+        }
+    }
+}
