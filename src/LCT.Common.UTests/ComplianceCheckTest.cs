1f6fef22fbd306a2d0d99df062fbc94991cb59af
harshitaharendra <95222790+harshitaharendra@users.noreply.github.com>
2025-08-22 10:44:46 +0530

    feat: UT for Compliance Exception. (#313)
    * UT for Compliance Exception.
    
    * Added IT testcase .
    
    * Integration test updated.
    
    * error fixed.
    
    ---------
    
    Co-authored-by: HARSHITA Singh <harshita.singh@siemens.com>

---
+++ src/LCT.Common.UTests/ComplianceCheckTest.cs
@@ -0,0 +1,142 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2025 Siemens AG
+//
+//  SPDX-License-Identifier: MIT
+// -------------------------------------------------------------------------------------------------------------------- 
+
+using File = System.IO.File;
+using LCT.Common.ComplianceValidator;
+using LCT.Common.Model;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LCT.Common.Tests.ComplianceValidator
+{
+    [TestFixture]
+    public class ComplianceCheckTests
+    {
+        private ComplianceCheck _complianceCheck;
+
+        [SetUp]
+        public void Setup()
+        {
+            _complianceCheck = new ComplianceCheck();
+        }
+
+        [Test]
+        public async Task LoadSettingsAsync_ValidJsonFile_ReturnsSettingsModel()
+        {
+            // Arrange
+            var settings = new ComplianceSettingsModel
+            {
+                ComplianceExceptionComponents = new List<ComplianceExceptionComponent>
+                {
+                    new ComplianceExceptionComponent
+                    {
+                        Id = "1",
+                        Name = "TestComp",
+                        Description = "desc",
+                        Purl = new List<string> { "pkg:maven/test@1.0.0" },
+                        ComplianceInstructions = new ComplianceInstructions
+                        {
+                            WarningMessage = "Warn",
+                            Recommendation = "Rec"
+                        }
+                    }
+                }
+            };
+            var tempFile = Path.GetTempFileName();
+            await File.WriteAllTextAsync(tempFile, JsonSerializer.Serialize(settings));
+
+            // Act
+            var result = await _complianceCheck.LoadSettingsAsync(tempFile);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.ComplianceExceptionComponents);
+            Assert.AreEqual("1", result.ComplianceExceptionComponents[0].Id);
+
+            File.Delete(tempFile);
+        }
+
+        [Test]
+        public void Check_NullSettingsOrData_ReturnsFalse()
+        {
+            Assert.IsFalse(_complianceCheck.Check(null, new List<ComparisonBomData>()));
+            Assert.IsFalse(_complianceCheck.Check(new ComplianceSettingsModel(), null));
+        }
+
+        [Test]
+        public void Check_DataNotListOfComparisonBomData_ReturnsFalse()
+        {
+            var settings = new ComplianceSettingsModel();
+            Assert.IsFalse(_complianceCheck.Check(settings, new object()));
+        }
+
+        [Test]
+        public void Check_NoWarnings_ReturnsTrue()
+        {
+            var settings = new ComplianceSettingsModel
+            {
+                ComplianceExceptionComponents = new List<ComplianceExceptionComponent>()
+            };
+            var data = new List<ComparisonBomData>();
+            Assert.IsTrue(_complianceCheck.Check(settings, data));
+        }
+
+        [Test]
+        public void Check_WithWarnings_ReturnsFalse()
+        {
+            var comp = new ComplianceExceptionComponent
+            {
+                Purl = new List<string> { "pkg:maven/test@1.0.0" },
+                ComplianceInstructions = new ComplianceInstructions
+                {
+                    WarningMessage = "Test warning",
+                    Recommendation = "Test recommendation"
+                }
+            };
+            var settings = new ComplianceSettingsModel
+            {
+                ComplianceExceptionComponents = new List<ComplianceExceptionComponent> { comp }
+            };
+            var data = new List<ComparisonBomData>
+            {
+                new ComparisonBomData { ComponentExternalId = "pkg:maven/test@1.0.0" }
+            };
+
+            Assert.IsFalse(_complianceCheck.Check(settings, data));
+            var warnings = _complianceCheck.GetResults();
+            Assert.IsTrue(warnings.Count > 0);
+            Assert.IsTrue(warnings[0].Contains("Test warning"));
+        }
+
+        [Test]
+        public void PrintRecommendation_NullOrWhitespace_DoesNothing()
+        {
+            Assert.DoesNotThrow(() => _complianceCheck.PrintRecommendation(null));
+            Assert.DoesNotThrow(() => _complianceCheck.PrintRecommendation(""));
+            Assert.DoesNotThrow(() => _complianceCheck.PrintRecommendation("   "));
+        }
+
+        [Test]
+        public void PrintWarning_NullOrWhitespace_DoesNothing()
+        {
+            Assert.DoesNotThrow(() => _complianceCheck.PrintWarning(null));
+            Assert.DoesNotThrow(() => _complianceCheck.PrintWarning(""));
+            Assert.DoesNotThrow(() => _complianceCheck.PrintWarning("   "));
+        }
+
+        [Test]
+        public void GetResults_ReturnsWarningsList()
+        {
+            var result = _complianceCheck.GetResults();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<string>>(result);
+        }
+    }
+}
+
---
+++ src/SW360IntegrationTest/Nuget/ComplianceCheckIntegrationNuget.cs
@@ -0,0 +1,113 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2025 Siemens AG
+//
+//  SPDX-License-Identifier: MIT
+
+// -------------------------------------------------------------------------------------------------------------------- 
+
+using CycloneDX.Models;
+using NUnit.Framework;
+using System.IO;
+using TestUtilities;
+using File = System.IO.File;
+
+
+namespace SW360IntegrationTest.Nuget
+{
+    [TestFixture, Order(43)]
+    public class ComplianceCheckIntegrationNuget
+    {
+        private TestParamNuget testParameters;
+        private string CCTComparisonBomTestFile { get; set; }
+        private string OutFolder { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            testParameters = new TestParamNuget();
+            OutFolder = TestHelper.OutFolder;
+            CCTComparisonBomTestFile = Path.GetFullPath(Path.Combine(OutFolder, "..", "..", "src", "SW360IntegrationTest", "PackageCreatorTestFiles", "Nuget", "CCTComplienceCheckComparisonBOMNugetInitial.json"));
+
+            if (!TestHelper.BOMCreated)
+            {
+                OutFolder = TestHelper.OutFolder;
+                string packagejsonPath = Path.GetFullPath(Path.Combine(OutFolder, "..", "..", "TestFiles", "IntegrationTestFiles", "SystemTest1stIterationData", "ComplienceCheckNugetFolder"));
+                string bomPath = Path.GetFullPath(Path.Combine(OutFolder, "..", "BOMs"));
+                TestHelper.RunBOMCreatorExe(new string[]{
+                TestConstant.PackageFilePath, packagejsonPath,
+                TestConstant.BomFolderPath, bomPath,
+                TestConstant.Sw360Token, testParameters.SW360AuthTokenValue,
+                TestConstant.SW360AuthTokenType, testParameters.SW360AuthTokenType,
+                TestConstant.SW360URL, testParameters.SW360URL,
+                TestConstant.SW360ProjectID, testParameters.SW360ProjectID,
+                TestConstant.SW360ProjectName, testParameters.SW360ProjectName,
+                TestConstant.JFrogApiURL, testParameters.JfrogApi,
+                TestConstant.JfrogNugetInternalRepo,"Nuget-test",
+                TestConstant.ArtifactoryKey, testParameters.ArtifactoryUploadApiKey,
+                TestConstant.TelemetryEnable, testParameters.TelemetryEnable,
+                TestConstant.ProjectType,"NUGET",
+                TestConstant.Mode,""});
+            }
+        }
+
+        [Test, Order(1)]
+        public void ComponentCreatorExe_ProvidedBOMFilePath_ReturnsSuccess()
+        {
+            string bomPath = Path.GetFullPath(Path.Combine(OutFolder, "..", "BOMs"));
+            // Assert
+            // Check exit is normal
+            Assert.AreEqual(0, TestHelper.RunComponentCreatorExe(new string[] {
+                TestConstant.BomFolderPath,bomPath,
+                TestConstant.Sw360Token, testParameters.SW360AuthTokenValue,
+                TestConstant.SW360URL, testParameters.SW360URL,
+                TestConstant.SW360AuthTokenType, testParameters.SW360AuthTokenType,
+                TestConstant.SW360ProjectID, testParameters.SW360ProjectID,
+                TestConstant.SW360ProjectName, testParameters.SW360ProjectName,
+                TestConstant.ProjectType,"NUGET",
+                TestConstant.FossologyURL, testParameters.FossUrl,
+                TestConstant.EnableFossologyTrigger,testParameters.FossologyTrigger,
+                TestConstant.TelemetryEnable, testParameters.TelemetryEnable,
+                TestConstant.Mode,""}),
+                "Test to run Package Creator EXE execution");
+        }
+
+        [Test, Order(2)]
+        public void TestComparisionBOMUpdation()
+        {
+            bool filecheck = false;
+
+            // Expected
+            ComponentJsonParsor expected = new ComponentJsonParsor();
+            expected.Read(CCTComparisonBomTestFile);
+
+            // Actual
+            string generatedBOM = Path.GetFullPath(Path.Combine(OutFolder, "..", "BOMs", $"{testParameters.SW360ProjectName}_Bom.cdx.json"));
+            if (File.Exists(generatedBOM))
+            {
+
+                filecheck = true;
+                ComponentJsonParsor actual = new ComponentJsonParsor();
+                actual.Read(generatedBOM);
+
+                foreach (var item in expected.Components)
+                {
+
+                    foreach (var i in actual.Components)
+                    {
+                        if ((i.Name == item.Name) && (i.Version == item.Version))
+                        {
+                            Component component = i;
+                            Assert.AreEqual(item.Name, component.Name);
+                            Assert.AreEqual(item.Version, component.Version);
+                            Assert.AreEqual(item.BomRef, component.BomRef);
+                            Assert.AreEqual(item.Purl, component.Purl);
+                            Assert.AreEqual(item.Properties.Count, component.Properties.Count);
+
+                        }
+                    }
+
+                }
+            }
+            Assert.IsTrue(filecheck, "CycloneDx BOM not exist");
+        }
+    }
+}
