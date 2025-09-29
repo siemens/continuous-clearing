f74b753368c5cf88495f737cb5224eaf7dcb286b
crvreddy <mfcwlragava@gmail.com>
2025-09-29 10:18:12 +0530

    improved code coverage

---
+++ src/LCT.Common.UTests/CommonAppSettingsTests.cs
@@ -0,0 +1,43 @@
+﻿using LCT.Common.Interface;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCT.Common.UTest
+{
+    [TestFixture]
+    public class CommonAppSettingsTests
+    {
+        private class TestDirectory(IEnvironmentHelper envHelper) : LCT.Common.Directory(envHelper)
+        {
+
+            // Override to always throw DirectoryNotFoundException
+            protected new void ValidateFolderPath(string value)
+            {
+                throw new DirectoryNotFoundException("Test exception");
+            }
+        }
+
+        [Test]
+        public void InputFolder_WhenDirectoryNotFoundException_LogsErrorAndCallsExit()
+        {
+            // Arrange
+            var envHelperMock = new Mock<IEnvironmentHelper>();
+            bool exitCalled = false;
+            envHelperMock.Setup(e => e.CallEnvironmentExit(It.IsAny<int>())).Callback(() => exitCalled = true);
+
+            var testDirectory = new TestDirectory(envHelperMock.Object);
+
+            // Act
+            testDirectory.InputFolder = "nonexistent_path";
+
+            // Assert
+            Assert.IsTrue(exitCalled, "Environment exit should be called on DirectoryNotFoundException.");
+        }
+    }
+}
--- src/LCT.Common/CommonAppSettings.cs
+++ src/LCT.Common/CommonAppSettings.cs
@@ -5,6 +5,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using LCT.Common.Constants;
+using LCT.Common.Interface;
 using LCT.Common.Model;
 using log4net;
 using System;
@@ -70,12 +71,14 @@ namespace LCT.Common
             }
 
         }
-    }    
+    }
+    [ExcludeFromCodeCoverage]
     public class Telemetry
     {
         public bool Enable { get; set; } = true;
         public string ApplicationInsightsConnectionString { get; set; }
     }
+    [ExcludeFromCodeCoverage]
     public class SW360
     {
         private string m_URL;
@@ -102,6 +105,7 @@ namespace LCT.Common
         }
 
     }
+    [ExcludeFromCodeCoverage]
     public class Fossology
     {
         private string m_FOSSURL;
@@ -146,13 +150,15 @@ namespace LCT.Common
 
         public bool DryRun { get; set; } = false;
     }
-    public class Directory
+    public class Directory(IEnvironmentHelper envHelper)
     {
         static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        private static readonly EnvironmentHelper environmentHelper = new EnvironmentHelper();
+        private readonly IEnvironmentHelper environmentHelper = envHelper;
         private string m_InputFolder;
         private string m_OutputFolder;
         private string m_LogFolder;
+
+        public Directory() : this(new EnvironmentHelper()) { }
 
         public string InputFolder
         {
