84f7bb173857f3ffc2630fbf803a2e7425d7d8b1
crvreddy <mfcwlragava@gmail.com>
2026-02-02 10:33:02 +0530

    testing for alpine

--- src/LCT.SW360PackageCreator.UTest/AlpinePackageDownloaderTest.cs
+++ src/LCT.SW360PackageCreator.UTest/AlpinePackageDownloaderTest.cs
@@ -25,7 +25,7 @@ namespace LCT.SW360PackageCreator.UTest
                 Name = "apk-tools",
                 Version = "2.12.9-r3",
                 SourceUrl = "https://gitlab.alpinelinux.org/alpine/apk-tools/-/archive/v2.12.9/apk-tools-v2.12.9.tar.gz",
-                AlpineSource = "https://gitlab.alpinelinux.org/alpine/apk-tools/-/archive/v$pkgver/apk-tools-v$pkgver.tar.gz\\n\\tfix-recursive-solve-1.patch\\n\\tfix-recursive-solve-2.patch\\n\\t_apk\\n\\t"
+                AlpineSource = "https://gitlab.alpinelinux.org/alpine/apk-tools/-/archive/v$pkgver/apk-tools-v$pkgver.tar.gz"
 
             };
             var localPathforDownload = $"{Directory.GetParent(Directory.GetCurrentDirectory())}\\ClearingTool\\DownloadedFiles\\";
